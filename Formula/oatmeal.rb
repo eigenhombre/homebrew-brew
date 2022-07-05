b65174968707c5c4024010ba3b7910c59cad6d0d
John Jacobsen <eigenhombre@gmail.com>
2022-07-05 17:31:51 -0500

    Fix version for test

--- Formula/oatmeal.rb
+++ Formula/oatmeal.rb
@@ -4,7 +4,7 @@ class Oatmeal < Formula
   license "EPL-2.0"
   url "https://github.com/eigenhombre/oatmeal/releases/download/v0.0.5/oatmeal-release.tgz"
   sha256 "fa27c3b95374357529488b8a41212e19841f7785b4c8bb5cc18f73b61ac91fce"
-  version "0.0.5"
+  version "0.0.6"
 
   uses_from_macos "ruby" => :build
 
