fd2a381272b24fcc1977f79dbac584fcf300854e
John Jacobsen <eigenhombre@gmail.com>
2022-07-06 07:59:59 -0500

    update formula

--- Formula/oatmeal.rb
+++ Formula/oatmeal.rb
@@ -2,9 +2,9 @@ class Oatmeal < Formula
   desc "Common Lisp template"
   homepage "https://github.com/eigenhombre/oatmeal"
   license "EPL-2.0"
-  url "https://github.com/eigenhombre/oatmeal/archive/refs/tags/v0.0.21.tar.gz"
-  sha256 "d78457437e49ad5a06b931041a27128a8f30425259673ca7fbf948415de4cbd5"
-  version "0.0.21"
+  url "https://github.com/eigenhombre/oatmeal/archive/refs/tags/v0.0.22.tar.gz"
+  sha256 "adac79f2c817afd9bf82ef927fc9082a274a442850227fe3eccfb598f66f2b54"
+  version "0.0.22"
 
   uses_from_macos "ruby" => :build
 
