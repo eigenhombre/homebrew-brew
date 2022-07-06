c1e30629f7fb63c1491dd31bf2295d7311d2132e
John Jacobsen <eigenhombre@gmail.com>
2022-07-05 20:32:58 -0500

    Try a version without hand-uploading a tarball.

--- Formula/oatmeal.rb
+++ Formula/oatmeal.rb
@@ -2,9 +2,11 @@ class Oatmeal < Formula
   desc "Common Lisp template"
   homepage "https://github.com/eigenhombre/oatmeal"
   license "EPL-2.0"
-  url "https://github.com/eigenhombre/oatmeal/releases/download/v0.0.7/oatmeal-release.tgz"
+  url "https://github.com/eigenhombre/oatmeal/archive/refs/tags/v0.0.8.tar.gz"
+  # url "https://github.com/eigenhombre/oatmeal/releases/download/v0.0.7/oatmeal-release.tgz"
   sha256 "2db825de454f9929467a7efaaf3ef0b8cac48553fd67e5278562f0df7f06816d"
-  version "0.0.7"
+  # version "0.0.7"
+  version "0.0.8"
 
   uses_from_macos "ruby" => :build
 
