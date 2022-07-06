16fbe25f2e4195f10e1700c0f6f92a2862c7435d
John Jacobsen <eigenhombre@gmail.com>
2022-07-05 21:14:53 -0500

    Automation is getting closer...

--- Formula/oatmeal.rb
+++ Formula/oatmeal.rb
@@ -2,9 +2,9 @@ class Oatmeal < Formula
   desc "Common Lisp template"
   homepage "https://github.com/eigenhombre/oatmeal"
   license "EPL-2.0"
-  url "https://github.com/eigenhombre/oatmeal/archive/refs/tags/v0.0.9.tar.gz"
-  sha256 "a2c49ef04d3b3754c6de7aaf809fb0c5f8bf0d63f8295785f9dd3977f6064443"
-  version "0.0.9"
+  url "https://github.com/eigenhombre/oatmeal/archive/refs/tags/v0.0.15.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "0.0.15"
 
   uses_from_macos "ruby" => :build
 
